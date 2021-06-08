e518d9844a8910f7ab87bc25669ebc572c1e2de8
collin <collin@bantam-tech.com>
2021-06-08 12:56:06 -0500

    1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -13,31 +13,39 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
 
-    //zero fill aray for votes
-    const initVotes = anecdotes.map(elem => 0)
+  //zero fill aray for votes
+  const initVotes = anecdotes.map(elem => 0)
 
-    const [selected, setSelected] = useState(0)
-    const [vote, setVote] = useState(initVotes)
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(initVotes)
+  const [mostVotes, setMostVotes] = useState(0)
   
-    let change
+  let change
   
-    const newAnecdote = () => {
-      change = () => Math.floor(Math.random() * anecdotes.length);
-      setSelected(change);
-    };
+  const newAnecdote = () => {
+    change = () => Math.floor(Math.random() * anecdotes.length);
+    setSelected(change);
+  };
   
-    const handleVote = () => {
-      const newVote = [...vote];
-      newVote[selected]++;
-      setVote(newVote)
+  const handleVote = () => {
+    const newVote = [...vote];
+    newVote[selected]++;
+    if (newVote[selected] > newVote[mostVotes]) {
+      setMostVotes(selected)
     }
+    setVote(newVote)
+  }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
+      <p>Has {vote[selected]} votes</p>
       <button onClick={handleVote}>Vote</button>
       <button onClick={newAnecdote}>Next anecdote</button>
-      <p>{vote[selected]}</p>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVotes]}</p>
+      <p>Has {vote[mostVotes]} votes</p>
     </div>
   )
 }
